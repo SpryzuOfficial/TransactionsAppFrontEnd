c4db248d93624ab84665456c87f22762070c1f00
SpryzuOfficial <spryzuofficial@gmail.com>
2023-03-18 10:46:12 -0600

    List items delete animations

--- src/dashboard/components/CategoryItem.jsx
+++ src/dashboard/components/CategoryItem.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 
@@ -6,9 +7,10 @@ import '../views/css/List.css';
 
 export const CategoryItem = ({ category, setStates }) => 
 {
+    const [isDelete, setIsDelete] = useState(false);
     const { startDeleteCategory } = useCategoriesStore();
     const { setState, setUpdate } = setStates;
-
+    
     const { id, name } = category;
     
     const editCategory = () =>
@@ -19,11 +21,12 @@ export const CategoryItem = ({ category, setStates }) =>
 
     const deleteCategory = () =>
     {
+        setIsDelete(true);
         startDeleteCategory({ id });
     }
 
     return (
-        <li className="list-element animate__animated animate__fadeInUp">
+        <li className={`list-element animate__animated animate__fadeInUp ${isDelete ? 'animate__fadeOutLeft' : ''}`}>
             <p>{ name }</p>
             <button className="list-element-btn edit-style" onClick={ editCategory }><EditIcon /></button>
             <button className="list-element-btn delete-style" onClick={ deleteCategory }><DeleteIcon /></button>
--- src/dashboard/components/TransactionItem.jsx
+++ src/dashboard/components/TransactionItem.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 import { useTransactionsStore } from '../../hooks/useTransactionsStore';
@@ -5,6 +6,7 @@ import '../views/css/List.css';
 
 export const TransactionItem = ({ transaction }) => 
 {
+    const [isDelete, setIsDelete] = useState(false);
     const { startDeleteTransaction } = useTransactionsStore();
 
     const { _id, name, amount, category, isSpent, date } = transaction;
@@ -14,11 +16,12 @@ export const TransactionItem = ({ transaction }) =>
 
     const deleteTransaction = () =>
     {
+        setIsDelete(true);
         startDeleteTransaction({ id: _id });
     }
 
     return (
-        <li className="list-element animate__animated animate__fadeInUp">
+        <li className={`list-element animate__animated animate__fadeInUp ${isDelete ? 'animate__fadeOutLeft' : ''}`}>
             <p>{ name }</p>
             <p className={`${isSpent ? 'text-red' : 'text-green'}`}>{`${isSpent ? '-' : '+'}$${ amount }`}</p>
             <p>Date: { dateObj.toLocaleDateString('en-US') }</p>
